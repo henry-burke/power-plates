4b7c0b23958f70eb4114bc6eed951ee7b21da5a4
ryanli <ryanli2925@gmail.com>
2024-11-04 21:14:36 -0600

    fixed login navigation and added fragment for beg, inter, and adv choices

---
+++ app/src/main/java/com/cs407/powerplates/ChoiceLevel.kt
@@ -0,0 +1,78 @@
+package com.cs407.powerplates
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cs407.lab5_milestone.UserState
+import com.cs407.lab5_milestone.UserViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ChoiceLevel (
+    private val injectedUserViewModel: UserViewModel? = null // For testing only
+): Fragment() {
+
+    private lateinit var userViewModel: UserViewModel
+
+    private lateinit var userPasswdKV: SharedPreferences
+
+    private var userId: Int = 0
+
+    private lateinit var greetingTextView: TextView
+    private lateinit var noteRecyclerView: RecyclerView
+    private lateinit var fab: FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //load user view model
+        userPasswdKV = requireContext().getSharedPreferences(
+            getString(R.string.userPasswdKV), Context.MODE_PRIVATE)
+
+        userViewModel = if (injectedUserViewModel != null) {
+            injectedUserViewModel
+        } else {
+            // TODO - Use ViewModelProvider to init UserViewModel
+            ViewModelProvider(requireActivity())[UserViewModel::class.java]
+        }
+        userId = userViewModel.userState.value.id
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_choice_level, container, false)
+        greetingTextView = view.findViewById(R.id.greetingTextView)
+       // noteRecyclerView = view.findViewById(R.id.noteRecyclerView)
+        //fab = view.findViewById(R.id.fab)
+        //return view
+        return view
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userState = userViewModel.userState.value
+        greetingTextView.text = getString(R.string.greeting_text, userState.name)
+
+
+    }
+
+
+}
--- app/src/main/java/com/cs407/powerplates/LoginFragment.kt
+++ app/src/main/java/com/cs407/powerplates/LoginFragment.kt
@@ -4,18 +4,20 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.cs407.lab5_milestone.UserState
 import com.cs407.lab5_milestone.UserViewModel
+import com.cs407.powerplates.R
+
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -34,7 +36,6 @@ class LoginFragment(
     private lateinit var userViewModel: UserViewModel
 
     private lateinit var userPasswdKV: SharedPreferences
-    //private lateinit var noteDB: NoteDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,19 +48,20 @@ class LoginFragment(
         loginButton = view.findViewById(R.id.loginButton)
         errorTextView = view.findViewById(R.id.errorTextView)
 
-        userViewModel = (if (injectedUserViewModel != null) {
+
+
+
+        userViewModel = if (injectedUserViewModel != null) {
             injectedUserViewModel
         } else {
             // TODO - Use ViewModelProvider to init UserViewModel
-            // LEARN and CLEAR all of this section about view modals and what is going on
-            ViewModelProvider(requireActivity()).get(UserViewModel::class.java)
-        })
-        //noteDB = NoteDatabase.getDatabase(requireContext())
+            ViewModelProvider(requireActivity())[UserViewModel::class.java]
+        }
 
         // TODO - Get shared preferences from using R.string.userPasswdKV as the name
-        // The thing !! does something check if it is necessary
-        this.userPasswdKV = activity?.getSharedPreferences(
-            getString(R.string.userPasswdKV), Context.MODE_PRIVATE)!!
+        userPasswdKV = requireActivity().getSharedPreferences(
+            getString(R.string.userPasswdKV),
+            Context.MODE_PRIVATE)
         return view
     }
 
@@ -76,25 +78,31 @@ class LoginFragment(
         // Set the login button click action
         loginButton.setOnClickListener {
             // TODO: Get the entered username and password from EditText fields
-            var userName = usernameEditText.text.toString()
-            var password = passwordEditText.text.toString()
-            CoroutineScope(Dispatchers.Default).launch{
-                if (getUserPasswd(userName, password)){
-                    // TODO: Set the logged-in user in the ViewModel (store user info) (placeholder)
-                    //var usersid = noteDB.userDao().getByName(userName).userId
-                    //userViewModel.setUser(UserState(usersid, userName, password)) // You will implement this in UserViewModel
-                    withContext(Dispatchers.Main){
-                        Log.i("info", "$userName")
-                        // TODO: Navigate to another fragment after successful login
-                        //findNavController().navigate(R.id.action_loginFragment_to_noteListFragment)
-                    }
+            val user = usernameEditText.text.toString()
+            val pass = passwordEditText.text.toString()
+            //Log.d("string check", user)
+            // TODO: Set the logged-in user in the ViewModel (store user info) (placeholder)
+            //userViewModel.setUser(UserState(0, user, pass)) // You will implement this in UserViewModel
+            // TODO: Navigate to another fragment after successful login
 
+            if (user.isEmpty() || pass.isEmpty()) {
+                errorTextView.visibility = View.VISIBLE
+                return@setOnClickListener
+            }
+
+            // Launch a coroutine to call the suspend function
+            CoroutineScope(Dispatchers.Main).launch {
+                val loginSuccessful = getUserPasswd(user, pass)
+
+                if (loginSuccessful) {
+                    // Navigate to another fragment after successful login
+                    //userViewModel.setUser(UserState(0, user, pass)) // Assuming you want to store an ID as well
+
+                    userViewModel.setUser(UserState(id, user, pass))
+                    findNavController().navigate(R.id.action_loginFragment_to_choiceLevelFragment)
                 } else {
-                    // TODO: Show an error message if either username or password is empty
-                    withContext(Dispatchers.Main){
-                        errorTextView.visibility = View.VISIBLE
-                        errorTextView.text = "Wrong Username or Password "
-                    }
+                    // Show an error message if login fails
+                    errorTextView.visibility = View.VISIBLE
                 }
             }
 
@@ -106,28 +114,40 @@ class LoginFragment(
         passwdPlain: String
     ): Boolean {
         // TODO: Hash the plain password using a secure hashing function
-        var pass = hash(passwdPlain)
+        val hashedInput = hash(passwdPlain)
+        // TODO: Check if the user exists in SharedPreferences (using the username as the key)
+        val sharedPref = activity?.getSharedPreferences(
+            getString(R.string.userPasswdKV), Context.MODE_PRIVATE)
 
-        // TODO: Check if the user exists in SharedPreferences (using the username as the key
-        if (userPasswdKV.contains(name)){
-            // TODO: Retrieve the stored password from SharedPreferences
-            val passwordStored = userPasswdKV.getString(name, null)
+        if(sharedPref?.contains(name) == true){
+            val findPassword = sharedPref.getString(name, "")
+            return findPassword == hashedInput
+        }
+        else{
+            val editor = sharedPref?.edit()
+            //hash password
+            editor?.putString(name, hashedInput)
+            editor?.apply()
 
-            // TODO: Compare the hashed password with the stored one and return false if they don't match
-            if (pass != passwordStored) {
-                return false
-            }
-        } // TODO: If the user doesn't exist in SharedPreferences, create a new user
-        else {
-            // TODO: Store the hashed password in SharedPreferences for future logins
-            with(userPasswdKV.edit()){
-                putString(name, pass)
-                apply()
-            }
+
+
+            return true
         }
 
+
+        // TODO: Retrieve the stored password from SharedPreferences
+
+        // TODO: Compare the hashed password with the stored one and return false if they don't match
+
+        // TODO: If the user doesn't exist in SharedPreferences, create a new user
+
+        // TODO: Insert the new user into the Room database (implement this in your User DAO)
+
+        // TODO: Store the hashed password in SharedPreferences for future logins
+
         // TODO: Return true if the user login is successful or the user was newly created
-        return true
+
+        //return true
     }
 
     private fun hash(input: String): String {
